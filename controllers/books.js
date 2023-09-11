13308c057df2a48e7953d4548ebc4f47738e7015
Michael Walters <michael.walters@infinityworks.com>
2023-09-11 15:16:20 +0100

    Add test data from local file

--- controllers/books.js
+++ controllers/books.js
@@ -1,9 +1,11 @@
+const books = require('../books.json');
+
 const getAllBooks = async (req, res) => {
     res.status(200).json({ msg: 'books route' });
 }
 
 const getAllBooksStatic = async (req, res) => {
-    res.status(200).json({ msg: 'books testing route' });
+    res.status(200).json({ books });
 }
 
 module.exports = {
