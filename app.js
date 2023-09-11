e6f7b46c9cb7319fe067a328c04d48e4f470d48d
Michael Walters <michael.walters@infinityworks.com>
2023-09-11 16:04:30 +0100

    Catch 404s and generic API errors

--- app.js
+++ app.js
@@ -4,12 +4,18 @@ const PORT = 3000;
 
 const booksRouter = require('./routes/books');
 
+const notFoundMiddleware = require('./middleware/not-found');
+const errorMiddleware = require('./middleware/error-handler');
+
 // routes
 app.get('/', (req, res) => {
     res.send('<h1>Books API</h1><a href="/api/v1/books">books route</a>');
 })
 
 app.use('/api/v1/books', booksRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
 
 const start = async () => {
     try {
